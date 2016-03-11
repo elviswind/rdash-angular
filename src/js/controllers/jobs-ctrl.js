fc0cdca2d72b5ff9ae67a37c43b6549150e4560d
junnan.wang@ef.com <junnan.wang@ef.com>
2016-03-11 15:57:34 +0800

    1

--- src/js/controllers/jobs-ctrl.js
+++ src/js/controllers/jobs-ctrl.js
@@ -2,12 +2,9 @@ angular
     .module('RDash')
     .controller('JobsCtrl', ['$scope', '$http', JobsCtrl]);
 
-var apiServer = "http://s.junnan.wang";
 function JobsCtrl($scope, $http) {
-    console.log('running');
     $scope.RefreshJobs = function() {
-        console.log('abc');
-        $http.get(apiServer + '/jobs').success(function(data) {
+        $http.get('/jobs').success(function(data) {
             for (var i = 0; i < data.length; i++) {
                 data[i].activeClass = data[i].active ? "active" : "";
                 data[i].activeText = data[i].active ? "运行中" : "未运行";
@@ -16,4 +13,27 @@ function JobsCtrl($scope, $http) {
         });
     };
     $scope.RefreshJobs();
+
+    $scope.editjob = function(x) {
+    };
+    $scope.activejob = function(job) {
+        $http.post('/active/' + job.name + '/' + (job.active + 1) % 2).success(function() {
+            $scope.RefreshJobs();
+        });
+    };
+    $scope.resetjob = function(name) {
+        $http.post('/reset/' + name).success(function() {
+            $scope.RefreshJobs();
+        });
+    };
+    $scope.deletejob = function(name) {
+        $http.post('/delete/' + name).success(function() {
+            $scope.RefreshJobs();
+        });
+    };
+    $scope.startjob = function(name) {
+        $http.post('/start/' + name).success(function() {
+            $scope.RefreshJobs();
+        });
+    };
 }

d9ba252d076aadaf297c13514047d8e21db790fe
junnan.wang@ef.com <junnan.wang@ef.com>
2016-04-06 17:55:09 +0800

    1

--- src/js/controllers/newmonitor-ctrl.js
+++ src/js/controllers/newmonitor-ctrl.js
@@ -4,13 +4,20 @@ angular
 
 function NewMonitorCtrl($scope, $http, $location, $stateParams, util) {
     var key = $stateParams.key;
+    var step = 1;
     if (key == "edit" && util.currentMonitor) {
         $scope.monitor = util.currentMonitor;
+        step = 2;
     } else if (key == "new") {
         $scope.monitor = {};
         $scope.monitor.action = monitorActionExample.toString();
         $scope.monitor.interval = 10;
+        step = 1;
     }
+    $scope.visible = {
+        step: step
+    };
+
     $scope.save = function() {
         $http.post('/savemonitor', $scope.monitor).success(function(data) {
             $scope.monitor = {};

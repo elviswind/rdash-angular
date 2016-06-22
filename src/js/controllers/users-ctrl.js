4dc0ac1f4a60fd3cc83f9efbade9c13c6cd12141
xman <x@man.com>
2016-06-22 16:56:11 +0800

    1

--- src/js/controllers/users-ctrl.js
+++ src/js/controllers/users-ctrl.js
@@ -3,5 +3,5 @@ angular
     .controller('UsersCtrl', ['$scope', '$http', 'util', '$location', UsersCtrl]);
 
 function UsersCtrl($scope, $http, util, $location) {
-    util.InjectListCrudScope('users', 'user', $scope, $http, $location, {isAdmin: false});
+    util.InjectListCrudScope('users', 'user', $scope, $http, $location, {isAdmin: true});
 }

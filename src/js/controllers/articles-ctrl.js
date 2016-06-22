8545a74cb5d5759e533b64f8cdc2f104fffa4669
xman <x@man.com>
2016-06-22 13:25:52 +0800

    1

--- src/js/controllers/articles-ctrl.js
+++
@@ -1,7 +0,0 @@
-angular
-    .module('RDash')
-    .controller('ArticlesCtrl', ['$scope', '$http', 'util', '$location', ArticlesCtrl]);
-
-function ArticlesCtrl($scope, $http, util, $location) {
-    util.InjectListCrudScope('articles', 'article', $scope, $http, $location, {isAdmin: false});
-}

d1ccd729b19411e6518bbd2bb6963c782d58ea67
xman <x@man.com>
2016-12-02 09:16:25 +0000

    1

--- src/js/controllers/jobs-ctrl.js
+++ src/js/controllers/jobs-ctrl.js
@@ -5,13 +5,20 @@ angular
 function JobsCtrl($scope, $http, $location, util) {
     $scope.RefreshJobs = function() {
       console.log('a');
-        $scope.jobs = null;
+        $scope.j1 = null;
+        $scope.j2 = null;
         $http.get('/suapi/jobs').success(function(data) {
+            var j1 = [], j2 = [];
             for (var i = 0; i < data.length; i++) {
                 data[i].activeClass = data[i].active ? "active" : "";
                 data[i].activeText = data[i].active ? "on" : "off";
+                if(data[i].active){
+                  j1.push(data[i])
+                }
+                else j2.push(data[i])
             }
-            $scope.jobs = data;
+            $scope.j1 = j1;
+            $scope.j2 = j2;
             util.allJobs = data;
         });
     };
